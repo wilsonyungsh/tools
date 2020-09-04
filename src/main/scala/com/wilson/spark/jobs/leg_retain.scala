3119820066b9936cbf4cdd528f3c80917c5527d7
wilsonyung <wilson0313@gmail.com>
2020-09-04 17:45:38 +1000

    leg-retain scripts

--- src/main/scala/com/wilson/spark/jobs/leg_retain.scala
+++ src/main/scala/com/wilson/spark/jobs/leg_retain.scala
@@ -9,7 +9,7 @@ object leg_retain {
     // check for args availability before getting paths
     val (period) =
       args.size match {
-        case x if x == 1 => (args(0))   // check input parameters
+        case x if x == 1 => (args(0)) // check input parameters
         case _ => {
           sys.error("Invalid arguments. Please pass (1) YYYYMM")
           sys.exit(1)
@@ -21,15 +21,16 @@ object leg_retain {
     val base_path = "s3a://au-daas-users/wilson/tfnsw/walkleg_trip/nsw_base/"
     val output_path = "s3a://au-daas-users/wilson/tfnsw/walkleg_trip/legs/"
     val trip = spark.read.parquet(base_path + period + "*")
-      .select("startTimeConverted", "agentId", "dominantMode", "origin_state","origin_gcc", "origin_sa4",
-        "destination_state", "destination_gcc", "destination_sa4","linksInfo","distance", "weight","listOfModes","listofLinks")
-      .withColumn("retain",when(size('listOfModes) < 2,lit("1")).otherwise("0"))
-      .drop("listOfModes","linksInfo")
+      .select("startTimeConverted", "agentId", "dominantMode", "origin_state", "origin_gcc", "origin_sa4",
+        "destination_state", "destination_gcc", "destination_sa4", "linksInfo", "distance", "weight", "listOfModes", "listofLinks")
+      .withColumn("retain", when(size('listOfModes) < 2, lit("1")).otherwise("0"))
+      .drop("listOfModes", "linksInfo")
       .filter('retain === 1)
-      .withColumn("date",to_date('startTimeConverted))
+      .withColumn("date", to_date('startTimeConverted))
 
     //write out
     trip.write.parquet(output_path + "retained/" + period)
     //stop spark
     spark.stop
   }
+}
