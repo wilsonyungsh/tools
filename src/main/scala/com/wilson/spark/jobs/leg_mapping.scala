08548b49212fdd52ae4965a1e3023c7089645cae
wilsonyung <wilson0313@gmail.com>
2020-09-10 09:48:49 +1000

    rewrite leg mapping

--- src/main/scala/com/wilson/spark/jobs/leg_mapping.scala
+++ src/main/scala/com/wilson/spark/jobs/leg_mapping.scala
@@ -61,8 +61,9 @@ object leg_mapping {
     // join to master table based on uuid
     val full = leg.join(mapped_o,Seq("uuid"),"left").join(mapped_d,Seq("uuid"),"left").repartition(1024)
     //write out
-    val out_path = "s3a://au-daas-users/wilson/tfnsw/walkleg_trip/legs/monthly_leg_mapped/"
-    full.write.mode("overwrite").parquet(out_path + period)
+    //val out_path = "s3a://au-daas-users/wilson/tfnsw/walkleg_trip/legs/monthly_leg_mapped/"
+    val local_path = "/user/wilson/monthly_leg_mapped/"
+    full.write.mode("overwrite").parquet(local_path + period)
     //stop spark
     spark.stop
   }
